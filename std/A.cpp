4c134b958ffdc8e76b69d75af03b7c6058f13b42
AgOH <stagoh@qq.com>
2021-12-19 20:46:00 +0800

    feat: use xcode minted style

--- std/A.cpp
+++ std/A.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
-using namespace std;
-int main()
+#include <iostream>     // 头文件
+using namespace std;    // 使用 std 命名空间
+int main()              // 主函数
 {
-    int a,b;
-    cin>>a>>b;
-    cout<<a+b<<endl;
-    return 0;
+    int a,b;            // 两个变量 a,b
+    cin>>a>>b;          // 读入
+    cout<<a+b<<endl;    // 输出
+    return 0;           // 返回 0 代表正常退出
 }
