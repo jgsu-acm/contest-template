e787d1ab9dd4fd419db1639a4dd8f8dd0e364e84
StableAgOH <stagoh17@gmail.com>
2022-05-03 21:11:24 +0800

    feat: move cls to submodule

--- std/A.cpp
+++ std/A.cpp
@@ -1,9 +1,11 @@
-#include <iostream>     // 头文件
-using namespace std;    // 使用 std 命名空间
-int main()              // 主函数
+/* 中文注释 */
+// 中文注释
+#include <iostream>
+using namespace std;
+int main()
 {
-    int a,b;            // 两个变量 a,b
-    cin>>a>>b;          // 读入
-    cout<<a+b<<endl;    // 输出
-    return 0;           // 返回 0 代表正常退出
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    return 0;
 }
